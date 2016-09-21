bb3e148ccecfed43be69a397dc607815448b27d0
Ruben Gomez <rgomez@emergya.com>
2016-09-21 16:25:08 +0200

    Revert "Create Extra fields user model and form."
    This reverts commit 8b27c5f1b3c47997e70fc027009b713e23db78d9.

--- lms/djangoapps/extra_info_user/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from extra_info_user.models import ExtraInfo
-
-
-class ExtraInfoUserForm(forms.ModelForm):
-
-    class Meta:
-        model = ExtraInfo
-        fields = ('educational_centre_code', )
--- lms/djangoapps/extra_info_user/models.py
+++ lms/djangoapps/extra_info_user/models.py
@@ -1,14 +1,3 @@
-from django.conf import settings
 from django.db import models
-from django.core.validators import RegexValidator
 
-USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
-
-
-class ExtraInfo(models.Model):
-    """
-    This model contains extra fields that will be saved when a user registers.
-    The form that wraps this model is in the forms.py file.
-    """
-    user = models.OneToOneField(USER_MODEL, null=True)
-    educational_centre_code = models.CharField(max_length=8, validators=[RegexValidator(r'^\d{0,8}$')])
+# Create your models here.
